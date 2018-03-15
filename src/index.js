67c9aa98e6ae62059a61ba8dba9de152fd13ea08
Anastasiya957 <anastasiyabarysheva95@gmail.com>
2018-03-15 14:05:23 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,26 +1,41 @@
 class SmartCalculator {
   constructor(initialValue) {
     // your implementation
+    this.expression = String(initialValue);
   }
 
   add(number) {
     // your implementation
+    this.expression += '+' + String(number);
+    return this;
   }
   
   subtract(number) {
     // your implementation
+    this.expression += '-' + String(number);
+    return this;
   }
 
   multiply(number) {
     // your implementation
+    this.expression += '*' + String(number);
+    return this;
   }
 
   devide(number) {
     // your implementation
+    this.expression += '/' + String(number);
+    return this;
   }
 
   pow(number) {
     // your implementation
+    this.expression += '**' + String(number);
+    return this;
+  }
+
+  valueOf() {
+    return eval(this.expression);
   }
 }
 
